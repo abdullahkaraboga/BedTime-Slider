88a5e4208acf8a7a4ecfbd6e2341d03dc7a62d49
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-02-15 00:38:25 +0300

    Initial Commit

---
+++ BedTimeSlider/BedTimeSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BedTimeSliderApp.swift
+//  BedTimeSlider
+//
+//  Created by Abdullah Karaboğa on 15.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct BedTimeSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
